aadc60ff17a13b44b8aa3ffaa9bff933cb71beb4
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-16 23:21:25 -0300

    Adiciona estilos globais

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,5 +3,9 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dash</h1>
+    </div>
+  );
 }
