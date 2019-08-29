41d9091ed0e98ccfeba69554b760f46d482db59f
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-29 00:13:00 -0300

    Adiciona validacoes no form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-email válido')
+    .required('Email obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 // import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
-        <button type="button">Acessar</button>
+        <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
